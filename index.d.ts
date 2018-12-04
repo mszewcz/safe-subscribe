3a3d5aac2ff9770531c36d5f78d1cad0b95249fe
Michał Szewczyk <ms@msworks.pl>
2018-12-04 10:01:56 +0100

    Updated to ng-packagr

--- index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Subscription } from 'rxjs';
-export declare function safeSubscribe<T>(classRef: Object, next?: (value: T) => void, error?: (error: any) => void, complete?: () => void): Subscription;
-declare module 'rxjs/internal/Observable' {
-    interface Observable<T> {
-        safeSubscribe: typeof safeSubscribe;
-    }
-}
